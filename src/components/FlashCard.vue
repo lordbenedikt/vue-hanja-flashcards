d988ce66f8432d4334c352089f85a09eb2c92442
Benedikt Jensen <benedikt.jensen@mni.thm.de>
2024-02-04 15:39:57 +0100

    add mobile controls

--- src/components/FlashCard.vue
+++ src/components/FlashCard.vue
@@ -1,11 +1,19 @@
 <template>
-  <base-card :isFront="isFront" @mouseover="showBack" @mouseleave="showFront">
-    <div style="height:9em">
-        <div v-if="isFront" class="hanja">{{ hanja }}</div>
-        <div class="center" v-if="!isFront" style="padding:0">
-            <span class="pronunciation">{{ pronunciation }}</span><br>
-            <span class="meaning">{{ meaningEnglish }}</span><br>
-        </div>
+  <base-card
+    :isFront="isFront"
+    @mouseover="showBack"
+    @mouseleave="showFront"
+    @touchstart="showBackMobile"
+    @touchend="showBackMobile"
+  >
+    <div style="height: 9em">
+      <div v-if="isFront" class="hanja">{{ hanja }}</div>
+      <div class="center" v-if="!isFront" style="padding: 0">
+        <span class="pronunciation">{{ pronunciation }}</span
+        ><br />
+        <span class="meaning">{{ meaningEnglish }}</span
+        ><br />
+      </div>
     </div>
   </base-card>
   <base-button @click="nextWord">Next</base-button>
@@ -16,38 +24,55 @@ export default {
   props: ["vocabset"],
   watch: {
     vocabset() {
-      this.nextWord()
+      this.nextWord();
     },
   },
   mounted() {
-    this.nextWord()
+    this.nextWord();
   },
   data() {
     return {
-        hanja: "",
-        pronunciation: "",
-        meaningEnglish: "",
-        isFront: true,
-    }
+      hanja: "",
+      pronunciation: "",
+      meaningEnglish: "",
+      isFront: true,
+    };
   },
   methods: {
     showBack() {
+      if (!this.isMobile()) {
         this.isFront = false;
+      }
     },
     showFront() {
+      if (!this.isMobile()) {
         this.isFront = true;
+      }
+    },
+    showBackMobile() {
+      if (this.isMobile()) {
+        this.isFront = false;
+      }
+    },
+    showFrontMobile() {
+      if (this.isMobile()) {
+        this.isFront = true;
+      }
     },
     nextWord() {
-        this.isFront = true;
-        const word = this.randomWord();
-        this.hanja = word.substring(0, 1);
-        const info = word.substring(3, word.length-1);
-        const infoParts = info.split('-').map(part => part.trim());
-        this.pronunciation = infoParts[0];
-        this.meaningEnglish = infoParts[2];
+      this.isFront = true;
+      const word = this.randomWord();
+      this.hanja = word.substring(0, 1);
+      const info = word.substring(3, word.length - 1);
+      const infoParts = info.split("-").map((part) => part.trim());
+      this.pronunciation = infoParts[0];
+      this.meaningEnglish = infoParts[2];
     },
     randomWord() {
       return this.vocabset[Math.floor(Math.random() * this.vocabset.length)];
+    },
+    isMobile() {
+      return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
     },
   },
 };
@@ -55,20 +80,20 @@ export default {
 
 <style scoped>
 .hanja {
-    font-size: 6em;
+  font-size: 6em;
 }
 .pronunciation {
-    font-size: 2em;
+  font-size: 2em;
 }
 .meaning {
-    font-size: 1.5em;
+  font-size: 1.5em;
 }
 div.center {
-    position: relative;
-    top: 20%;
-    /* text-align: middle; */
-    /* vertical-align: baseline; */
-    margin: auto;
-    /* transform: translate(-50%, -50%); */
+  position: relative;
+  top: 20%;
+  /* text-align: middle; */
+  /* vertical-align: baseline; */
+  margin: auto;
+  /* transform: translate(-50%, -50%); */
 }
 </style>
